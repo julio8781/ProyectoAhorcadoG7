bfd59eacdf59afd28a500fa8fbc334077db56bbc
Tommy Vercetti <tomasguijarroferrandiz@gmail.com>
2024-03-20 22:12:10 +0100

    Añadir funcionalidades palabras y estilos

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,12 +1,36 @@
-'use strict';
+"use strict";
+import { palabras } from "./palabras.js";
+let numeroAleatorio;
+let palabra = "";
+let oculta = [];
+let hueco = document.getElementById("palabraOculta");
+let buttons = document.getElementsByClassName("letra");
+let intentos = 6;
 
 function generaPalabra() {
-    numeroAleatorio = +(Math.random() * (palabras.length - 1)).toFixed(0);
-    console.log(numeroAleatorio);
-    palabra = palabras[numeroAleatorio][0].toUpperCase();
-    console.log(palabra);
-};
-
-function Id( str ){
-    return document.getElementById( str );
+	numeroAleatorio = +(Math.random() * (palabras.length - 1)).toFixed(0);
+	palabra = palabras[numeroAleatorio][0].toUpperCase();
+	console.log(palabra);
 }
+
+function Id(str) {
+	return document.getElementById(str);
+}
+
+function pintarGuiones(num) {
+	for (let i = 0; i < num; i++) {
+		oculta[i] = "_";
+	}
+	hueco.innerHTML = oculta.join("");
+}
+
+export {
+	generaPalabra,
+	Id,
+	pintarGuiones,
+	numeroAleatorio,
+	palabra,
+	oculta,
+	hueco,
+	buttons,
+};
--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,40 @@
 "use strict";
-import {palabras} from './palabras.js'
-import {Id, generarPalabras} from './funciones.js'
+import { palabras } from "./palabras.js";
+import {
+	Id,
+	generaPalabra,
+	pintarGuiones,
+	numeroAleatorio,
+	palabra,
+	oculta,
+	hueco,
+	buttons,
+} from "./funciones.js";
 
-function pintarGuiones(num) {
-    for (let i = 0; i < num; i++) {
-        oculta[i] = "_";
-    }
-    hueco.innerHTML = oculta.join("");
+function inicio() {
+	generaPalabra();
+	pintarGuiones(palabra.length);
+	generaABC("a", "z");
+	intentos = 6;
+	if (Id("turnos")) {
+		Id("turnos").innerHTML = intentos;
+	}
 }
+
+function pista() {
+	Id("huecoPista").innerHTML = palabras[numeroAleatorio][1];
+	setTimeout(function () {
+		Id("huecoPista").innerHTML = "";
+	}, 5000);
+}
+
+Id("pista").addEventListener("click", pista);
+
+// Iniciar
+window.addEventListener("onload", inicio());
+
+/*
+1.- CSS modo responsive y comentario hoja style
+2.- Como implementar paneles
+3.- Como podemos usar una API con acentos (comparar palabras con acentos con una sin acento) "¿simular API?"
+*/
